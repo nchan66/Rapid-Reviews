4507296406349fe6d70caa1a964e8c0c357b4c01
nchan66 <50092289+nchan66@users.noreply.github.com>
2021-03-24 17:57:21 -0700

    first commit
    google oauth with passport,
    mongodb with mongoose to create user collections

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {    
+    googleClientID: '840957226633-bo3p0fi128ebj7vr6ojkrobfihg139fm.apps.googleusercontent.com',
+    googleClientSecret: '_np0OONY-6cf0F9dsyWU6a0t',
+    mongoURI: 'mongodb+srv://db_webapp:H0pinmyvan@webapp.qa1wb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    cookieKey: 'mah n wuuuurrrrdddd'
+};
